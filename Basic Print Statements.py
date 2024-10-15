f4ea5e51e39a60c91d22d62e86c31b4823f516aa
manurdbradley23 <marcos.brad2023@gmail.com>
2024-10-15 11:22:57 +0400

    Add files via upload

---
+++ Basic Print Statements.py
@@ -0,0 +1,83 @@
+#Basic Print Statements code by Brad Menard R. Marcos - Cybersecurity Yr. 1
+
+#1. Basic String Output
+#This outputs Hello, World!
+print("Hello, World!")
+
+#2. Simple Math Inside Print
+#Calculates math and prints.
+print(5 + 3)
+
+#3. Multiple Arguments in Print
+#Concatenations Python, is, and fun together in one statement.
+print("Python", "is", "fun!")
+
+#4. Print with Escape Characters
+#\ allows special characters inside of the string.
+print("She said, \"Hello!\"")
+
+#5. Newline Character
+#\n inputs an enter space
+print("First line\nSecond line")
+
+#6. Tab Character
+#\t gives empty space to space out words in between them.
+print("Item 1\tItem 2\tItem 3")
+
+#7. Print Multiple Data Types Together
+#, can concatenate string and integers but not math operations.
+print("The result is", 42)
+
+#8. Print a Single Number
+#Prints an integer
+print(12345)
+
+#9. Print a Decimal Number
+#Prints a float
+print(3.14159)
+
+#10. Print Special Characters
+#outputs Dollar sign: $
+print("Dollar sign: $")
+
+#11. Using the sep Parameter
+#Concatenates 3 strings as well as adding a seperator item between them.
+print("apple", "banana", "cherry", sep=", ")
+
+#12. Using the end Parameter
+#end= Is another way to add another separate string
+print("Hello,", end=" ")
+print("World!")
+
+#13. Print a Repeated String
+#* Repeats string 3 times
+print("Python! " * 3)
+
+#14. Print a Unicode Character
+#Prints a heart symbol
+print("\u2764")
+
+#15. Print Empty Line
+#prints nothing
+print()
+
+#16. Concatenating Strings Inside Print
+print("Hello " + "World!")
+
+#17. Print Boolean Values
+#Prints boolean values
+print(True)
+print(False)
+
+#18. Printing Exponential Notation
+#This prints integer with exponential notation added
+print(1.2e3) # Prints 1200.0
+
+#19. Print Multiline Text Using Triple Quotes
+#It prints multiple lines
+print("""This is a multiline
+print statement.""")
+
+#20. Print Using Escaped Backslash
+#Prints using escaped backslash
+print("This is a backslash: \\")
